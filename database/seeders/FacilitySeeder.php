c0db0c10408a827b2df55040fd08fd70f3c515fd
skepter <48717111+skepter@users.noreply.github.com>
2021-11-04 13:43:32 +0100

    feat(zarizeni): přidání obrázků a seedů k prostorům a využití

---
+++ database/seeders/FacilitySeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FacilitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dummyDesc = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Pellentesque ipsum. Nam sed tellus id magna elementum tincidunt. In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus. Aliquam ornare wisi eu metus. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Aenean vel massa quis mauris vehicula lacinia. Integer in sapien. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque ipsum. Morbi scelerisque luctus velit. Donec iaculis gravida nulla. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Aliquam erat volutpat. In sem justo, commodo ut, suscipit at, pharetra vitae, orci.";
+        $dummyDec2 = "
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam rhoncus aliquam metus. Proin mattis lacinia justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam ornare wisi eu metus. Nullam dapibus fermentum ipsum. Cras elementum. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Mauris metus. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Integer pellentesque quam vel velit. Mauris tincidunt sem sed arcu. Etiam neque. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Maecenas sollicitudin. Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Maecenas sollicitudin. Suspendisse nisl. \n
+
+        Fusce wisi. Et harum quidem rerum facilis est et expedita distinctio. Nulla non arcu lacinia neque faucibus fringilla. Et harum quidem rerum facilis est et expedita distinctio. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Mauris dictum facilisis augue. Nam sed tellus id magna elementum tincidunt. Nunc dapibus tortor vel mi dapibus sollicitudin. Nullam faucibus mi quis velit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Nunc tincidunt ante vitae massa. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Praesent id justo in neque elementum ultrices. Aliquam in lorem sit amet leo accumsan lacinia. Nullam dapibus fermentum ipsum. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Curabitur bibendum justo non orci. \n
+        
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Fusce consectetuer risus a nunc. Nullam sit amet magna in magna gravida vehicula. Fusce tellus odio, dapibus id fermentum quis, suscipit id erat. Vivamus luctus egestas leo. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. Integer pellentesque quam vel velit. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Et harum quidem rerum facilis est et expedita distinctio. Maecenas aliquet accumsan leo.";
+
+
+        $facilities = [
+            ['id' => 1, 'name' => 'Koncertní a divadelní sály', 'description' => $dummyDesc],
+            ['id' => 2, 'name' => 'Relaxace', 'description' => $dummyDec2],
+            ['id' => 3, 'name' => 'Sportoviště a volný čas', 'description' => $dummyDesc],
+            ['id' => 4, 'name' => 'Svatba', 'description' => $dummyDec2],
+            ['id' => 5, 'name' => 'Ubytování', 'description' => $dummyDesc]
+        ];
+
+        DB::table('facility')->insert($facilities);
+    }
+}
--- database/seeders/StaticFileSeeder.php
+++ database/seeders/StaticFileSeeder.php
@@ -15,7 +15,7 @@ class StaticFileSeeder extends Seeder
 
     public function run()
     {
-        $staticFiles = [
+        $eventStaticFiles = [
             ['id' => 1, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'events', 'name' => 'img1', 'event_id' => 1],
             ['id' => 2, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'events', 'name' => 'img2', 'event_id' => 2],
             ['id' => 3, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'events', 'name' => 'img3', 'event_id' => 3],
@@ -23,6 +23,34 @@ class StaticFileSeeder extends Seeder
             ['id' => 5, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'events', 'name' => 'img5', 'event_id' => 5],
         ];
 
-        DB::table('static_file')->insert($staticFiles);
+        $facilityStaticFiles = [
+            ['id' => 6, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'img6', 'facility_id' => 1],
+            ['id' => 7, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'img7', 'facility_id' => 1],
+            ['id' => 8, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'img8', 'facility_id' => 1],
+            ['id' => 9, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'img9', 'facility_id' => 2],
+            ['id' => 10, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'img10', 'facility_id' => 2],
+            ['id' => 11, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'img11', 'facility_id' => 2],
+            ['id' => 12, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'img12', 'facility_id' => 2],
+            ['id' => 13, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'img13', 'facility_id' => 2],
+            ['id' => 14, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'img14', 'facility_id' => 1],
+            ['id' => 15, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Beach volleyball',  'facility_id' => 3],
+            ['id' => 16, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Golfové hřiště',  'facility_id' => 3],
+            ['id' => 17, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Posilovna',  'facility_id' => 3],
+            ['id' => 18, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Vnitřní bazén',  'facility_id' => 3],
+            ['id' => 19, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Kulečník',  'facility_id' => 3],
+            ['id' => 20, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Svatební tabule 1',  'facility_id' => 4],
+            ['id' => 21, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Obřad',  'facility_id' => 4],
+            ['id' => 22, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Obřad dekorace',  'facility_id' => 4],
+            ['id' => 23, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Svatební tabule 2',  'facility_id' => 4],
+            ['id' => 24, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Vnitřní obřadní místnost',  'facility_id' => 4],
+            ['id' => 25, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Koupelna 2',  'facility_id' => 5],
+            ['id' => 26, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Koupelna',  'facility_id' => 5],
+            ['id' => 27, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Ložnice 2',  'facility_id' => 5],
+            ['id' => 28, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Ložnice 3',  'facility_id' => 5],
+            ['id' => 29, 'mime_type' => 'application/jpg', 'extension' => '.jpg', 'folder_name' => 'facilities', 'name' => 'Ložnice',  'facility_id' => 5],
+        ];
+
+        DB::table('static_file')->insert($eventStaticFiles);
+        DB::table('static_file')->insert($facilityStaticFiles);
     }
 }
