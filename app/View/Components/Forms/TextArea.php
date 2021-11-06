1e8757b430347defd7e874e74446706c4aff2639
skepter <48717111+skepter@users.noreply.github.com>
2021-11-06 22:08:51 +0100

    refactor(reviews): logika form komponent

---
+++ app/View/Components/Forms/TextArea.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\View\Components\Forms;
+
+use Illuminate\View\Component;
+
+class TextArea extends Component
+{
+    public $labelText;
+    public $inputId;
+    public $placeholder;
+    public $inputName;
+
+    /**
+     * Create a new component instance.
+     *
+     * @param string $labelText
+     * @param string $inputId
+     * @param string $placeholder
+     * @param string $inputName
+     *
+     * @return void
+     */
+
+    public function __construct(string $labelText, string $inputId, string $placeholder, string $inputName)
+    {
+        $this->labelText = $labelText;
+        $this->inputId = $inputId;
+        $this->placeholder = $placeholder;
+        $this->inputName = $inputName;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.forms.text-area');
+    }
+}
