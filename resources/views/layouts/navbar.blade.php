86f37ed6658c92238de2c63dffc2ee8fe7bbb8bc
skepter <48717111+skepter@users.noreply.github.com>
2021-11-06 22:07:48 +0100

    style(reviews): aplikace formátování

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,13 +2,18 @@
     <nav class="main-nav">
         <div class="mobile-navbar-controls-wrapper" id="app-logo-wrapper">
             <a class="main-nav__heading-link" href="/">
-
-                <img class="main-nav__logo" src="/images/app-logo.png" alt="zámeček" />
-                <h1 class="main-nav__heading">
-                    Zámeček
-                </h1>
+                <img
+                    class="main-nav__logo"
+                    src="/images/app-logo.png"
+                    alt="zámeček"
+                />
+                <h1 class="main-nav__heading">Zámeček</h1>
             </a>
-            <button class="burger-bar" onclick="toggleNavbar()" aria-label="otevřít menu">
+            <button
+                class="burger-bar"
+                onclick="toggleNavbar()"
+                aria-label="otevřít menu"
+            >
                 {{-- <i class="fa fa-bars" aria-hidden="true"></i>
                 <i class="fa fa-times close" aria-hidden="true"></i>
                 x --}}
@@ -18,19 +23,39 @@
         </div>
         <ul class="main-nav__items">
             <li class="main-nav__item">
-                <a class="main-nav__link {{(request()->is('home*') ? 'main-nav__link--active' : '')}}" href="/">Domů</a>
+                <a
+                    class="main-nav__link {{(request()->is('home*') ? 'main-nav__link--active' : '')}}"
+                    href="/"
+                    >Domů</a
+                >
             </li>
             <li class="main-nav__item">
-                <a class="main-nav__link {{(request()->is('events*') ? 'main-nav__link--active' : '')}}" href="/events">Akce</a>
+                <a
+                    class="main-nav__link {{(request()->is('events*') ? 'main-nav__link--active' : '')}}"
+                    href="/events"
+                    >Akce</a
+                >
             </li>
             <li class="main-nav__item">
-                <a class="main-nav__link {{(request()->is('facilities*') ? 'main-nav__link--active' : '')}}" href="/facilities">Zařízení</a>
+                <a
+                    class="main-nav__link {{(request()->is('facilities*') ? 'main-nav__link--active' : '')}}"
+                    href="/facilities"
+                    >Zařízení</a
+                >
             </li>
             <li class="main-nav__item">
-                <a class="main-nav__link {{(request()->is('about*') ? 'main-nav__link--active' : '')}}" href="/about">Recenze</a>
+                <a
+                    class="main-nav__link {{(request()->is('reviews*') ? 'main-nav__link--active' : '')}}"
+                    href="/reviews"
+                    >Recenze</a
+                >
             </li>
             <li class="main-nav__item">
-                <a class="main-nav__link {{(request()->is('contact*') ? 'main-nav__link--active' : '')}}" href="/contact">Kontakt</a>
+                <a
+                    class="main-nav__link {{(request()->is('contact*') ? 'main-nav__link--active' : '')}}"
+                    href="/contact"
+                    >Kontakt</a
+                >
             </li>
         </ul>
     </nav>
