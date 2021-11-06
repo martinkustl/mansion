479c46db071b2a061f80282671f29cca10671757
skepter <48717111+skepter@users.noreply.github.com>
2021-11-06 22:06:20 +0100

    feat(reviews): tvorba form komponent

---
+++ resources/views/components/forms/text-input.blade.php
@@ -0,0 +1,10 @@
+<div class="form-group row">
+    <label for={{$inputId}} class="col-sm-1 col-form-label c-review-form--label">{{$labelText}}</label>
+    <div class="col-sm-11">
+        <input type="text" class="form-control"
+               id={{$inputId}} name={{$inputName}} placeholder={{$placeholder}} required>
+    </div>
+</div>
+@error($inputName)
+<div class="alert alert-danger">{{ $message }}</div>
+@enderror
