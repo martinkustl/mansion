d847f7c2c85ca6122ffb14620c13033c664bc4ee
skepter <48717111+skepter@users.noreply.github.com>
2021-11-07 15:54:05 +0100

    feat(contacts): přidání stránky kontakty

--- resources/views/facilities/facilities.blade.php
+++ resources/views/facilities/facilities.blade.php
@@ -1,23 +1,25 @@
 @extends ('layouts.page')
 
 @section('title')
-Zařízení
+    Zařízení
 @endsection
 
 @section('content')
 
 @section('pageHeading')
-Prostory a vyžití
+    Prostory a vyžití
 @endsection
 {{-- <x-event-list :events="$events" basePath="home"/> --}}
-<ul class="event-list">
+<ul class="items-list">
     @foreach ($facilities as $facility)
-    {{-- Cesta k obrázku je tvořena jako /folderName/id obrázku.extension --}}
-    <a class="event-list__link" href="facilities/{{$facility->id}}"> 
-        <article>
-            <x-facility-card :name="$facility->name" :description="$facility->description" :imgPath="$facility->folderName.'/'.$facility->staticFileId.$facility->extension" :imgName="$facility->imgName"/>
-        </article>
-    </a>
+        {{-- Cesta k obrázku je tvořena jako /folderName/id obrázku.extension --}}
+        <li>
+            <a class="event-list__link" href="facilities/{{$facility->id}}">
+                <x-facility-card :name="$facility->name" :description="$facility->description"
+                                 :imgPath="$facility->folderName.'/'.$facility->staticFileId.$facility->extension"
+                                 :imgName="$facility->imgName"/>
+            </a>
+        </li>
     @endforeach
 </ul>
 
