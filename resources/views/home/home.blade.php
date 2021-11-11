ca4c77acc1a65166b6ea034f41d05350a6ca267d
skepter <48717111+skepter@users.noreply.github.com>
2021-11-11 15:07:14 +0100

    feat(real-data-a-male-upravy): přidání link na nadcházející eventy

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,16 +1,20 @@
 @extends ('layouts.page')
 
 @section('title')
-Domovská stránka
+    Domovská stránka
 @endsection
 
 @section('content')
 
 @section('pageHeading')
-Co se u nás dělo
+    Co se u nás dělo
 @endsection
 
 {{-- Vykreslení eventů --}}
 <x-event-list :events="$events" basePath="home"/>
+
+<a class="events--link" href="/events">
+    Více o nadcházejících akcích najdete na tomto odkazu
+</a>
 
 @endsection
