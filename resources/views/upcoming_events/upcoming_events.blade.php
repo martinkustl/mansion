70f5e8464cb2a443798f9b77193494daaf3a8a55
skepter <48717111+skepter@users.noreply.github.com>
2021-11-17 00:04:30 +0100

    feat(pridani-admin-eventu): schování formu pro přidání eventu pro nepřihlášené uživatele

--- resources/views/upcoming_events/upcoming_events.blade.php
+++ resources/views/upcoming_events/upcoming_events.blade.php
@@ -61,61 +61,61 @@
 </x-centered-modal>
 
 @auth
-    <a href="/events/new">
-        Přidat nový event
-    </a>
-@endauth
-
-<form class="c-form c-new-event--form mb-3" action="/events" method="POST" enctype="multipart/form-data"
-      id="new-event-form">
-    @csrf
-    <div class="row">
-        <div class="col-md-4 mb-3 mb-md-0">
-            <label id="eventImageLabel" for="eventImage" class="w-100 h-100"
-                   style="cursor: pointer; border: 1px dashed black; display: flex; align-items: center; justify-content: center; position: relative; min-height: 200px">
+    <form class="c-form c-new-event--form mb-3" action="/events" method="POST" enctype="multipart/form-data"
+          id="new-event-form">
+        @csrf
+        <div class="row">
+            <div class="col-md-4 mb-3 mb-md-0">
+                <label id="eventImageLabel" for="eventImage" class="w-100 h-100"
+                       style="cursor: pointer; border: 1px dashed black; display: flex; align-items: center; justify-content: center; position: relative; min-height: 200px">
                 <span>
                     Nahrajte obrázek akce
                 </span>
-            </label>
-            <input type="file" name="eventImage" id="eventImage" onchange="readInput(this)"
-                   style="position: absolute; opacity:0; z-index: -1">
+                </label>
+                <input type="file" name="eventImage" id="eventImage" onchange="readInput(this)"
+                       style="position: absolute; opacity:0; z-index: -1">
 
-            @error("eventImage")
-            <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
+                @error("eventImage")
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="col-md-8 c-form--inputs-wrapper">
+                <x-forms.input inputType="text" labelText="Název" placeholder="Název" inputId="title"
+                               inputName="title"/>
+                <fieldset class="d-flex align-items-center flex-row c-radios-wrapper">
+                    <legend class="w-auto" style="white-space: nowrap; font-size: 1rem; margin-bottom: 0;">Typ akce
+                    </legend>
+                    <div class="d-flex w-100 align-items-center flex-row c-radios-wrapper">
+                        @foreach($eventTypes as $eventType)
+                            <div class="form-check">
+                                <input class="form-check-input c-radio-button" type="radio"
+                                       id="{{$eventType->type}}" name="eventType" value="{{$eventType->id}}"
+                                >
+                                <label class="form-check-label" for="{{$eventType->type}}">
+                                    {{$eventType->name}}
+                                </label>
+                            </div>
+                        @endforeach
+                        @error("eventType")
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </fieldset>
+                <x-forms.input inputType="date" labelText="Datum" placeholder="Datum ve formátu DD.MM.YYYY"
+                               inputId="date"
+                               inputName="date"/>
+                <x-forms.input inputType="number" labelText="Cena" placeholder="Cena (pouze číslo)" inputId="price"
+                               inputName="price"/>
+                <x-forms.text-area labelText="Popis" placeholder="Popis" inputId="description" inputName="description"/>
+            </div>
         </div>
-        <div class="col-md-8 c-form--inputs-wrapper">
-            <x-forms.input inputType="text" labelText="Název" placeholder="Název" inputId="title" inputName="title"/>
-            <fieldset class="d-flex align-items-center flex-row c-radios-wrapper">
-                <legend class="w-auto" style="white-space: nowrap; font-size: 1rem; margin-bottom: 0;">Typ akce</legend>
-                <div class="d-flex w-100 align-items-center flex-row c-radios-wrapper">
-                    @foreach($eventTypes as $eventType)
-                        <div class="form-check">
-                            <input class="form-check-input c-radio-button" type="radio"
-                                   id="{{$eventType->type}}" name="eventType" value="{{$eventType->id}}"
-                            >
-                            <label class="form-check-label" for="{{$eventType->type}}">
-                                {{$eventType->name}}
-                            </label>
-                        </div>
-                    @endforeach
-                    @error("eventType")
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-            </fieldset>
-            <x-forms.input inputType="date" labelText="Datum" placeholder="Datum ve formátu DD.MM.YYYY"
-                           inputId="date"
-                           inputName="date"/>
-            <x-forms.input inputType="number" labelText="Cena" placeholder="Cena (pouze číslo)" inputId="price"
-                           inputName="price"/>
-            <x-forms.text-area labelText="Popis" placeholder="Popis" inputId="description" inputName="description"/>
+        <div class="d-flex justify-content-end w-100 mt-3">
+            <x-forms.submit-button btnText="Vytvořit"/>
         </div>
-    </div>
-    <div class="d-flex justify-content-end w-100 mt-3">
-        <x-forms.submit-button btnText="Vytvořit"/>
-    </div>
-</form>
+    </form>
+@endauth
+
+
 
 {{-- Vykreslení eventů --}}
 <x-event-list :events="$events" basePath="events"/>
